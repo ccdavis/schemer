2f6399540594f680f5376ba34b0ea406b383c6c9
Colin Davis <colin.c.davis@gmail.com>
2021-07-09 15:07:47 -0700

    Added subtract, multiply, divide operator support

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -34,6 +34,109 @@ use crate::list::List;
 			} // Some(right_value)
 		} // match					
 	}
+
+
+	fn subtract(list:List)->Result<SExpression,String>{			
+		subtract_from(Cell::Int(0), list)
+	}
+	
+	fn subtract_from(left_value:Cell, list:List)-> Result<SExpression, String>{		
+		match list.first().eval_as_number(){
+			Err(message)=>{
+				Err(message)
+			},
+			Ok(right_value) =>{			
+				
+				let partial_sum = match (left_value, right_value) {			
+					(Cell::Int(l),Cell::Int(r)) => Cell::Int(l - r),
+					(Cell::Int(l), Cell::Flt(r)) => Cell::Flt(l as f64 - r),
+					(Cell::Flt(l), Cell::Int(r)) => Cell::Flt(l - r as f64),
+					(Cell::Flt(l), Cell::Flt(r)) => Cell::Flt(l - r),
+					_ => {				
+						// Type error:
+						panic!("Data type error. Type checking should have caught this earlier.");
+					},
+				}; // match					
+				if list.rest().is_empty(){
+					Ok(SExpression::Cell(partial_sum))
+				}else{			
+					subtract_from(partial_sum, list.rest())		
+				}
+			} // Some(right_value)
+		} // match					
+	}
+
+
+
+	fn multiply(list:List)->Result<SExpression,String>{			
+		multiply_by(Cell::Int(1), list)
+	}
+	
+	fn multiply_by(left_value:Cell, list:List)-> Result<SExpression, String>{		
+		match list.first().eval_as_number(){
+			Err(message)=>{
+				Err(message)
+			},
+			Ok(right_value) =>{							
+				let partial_product = match (left_value, right_value) {			
+					(Cell::Int(l),Cell::Int(r)) => Cell::Int(l * r),
+					(Cell::Int(l), Cell::Flt(r)) => Cell::Flt(l as f64 * r),
+					(Cell::Flt(l), Cell::Int(r)) => Cell::Flt(l * r as f64),
+					(Cell::Flt(l), Cell::Flt(r)) => Cell::Flt(l * r),
+					_ => {				
+						// Type error:
+						panic!("Data type error. Type checking should have caught this earlier.");
+					},
+				}; // match					
+				if list.rest().is_empty(){
+					Ok(SExpression::Cell(partial_product))
+				}else{			
+					multiply_by(partial_product, list.rest())		
+				}
+			} // Some(right_value)
+		} // match					
+	}
+
+
+
+	fn divide(list:List)->Result<SExpression,String>{			
+		// If there's only one argument to /
+		if list.rest().is_empty(){
+			divide_into(Cell::Int(1), list)		
+		}else{
+			match list.first().eval_as_number(){
+				Ok(numerator) => divide_into(numerator, list.rest()),
+				Err(message) => Err(message),
+			} // match				
+		} // else
+	}
+	
+	fn divide_into(numerator_value:Cell, list:List)-> Result<SExpression, String>{		
+		match list.first().eval_as_number(){
+			Err(message)=>{
+				Err(message)
+			},
+			Ok(denominator_value) =>{							
+				let partial_product = match (numerator_value, denominator_value) {			
+					(Cell::Int(n),Cell::Int(d)) => Cell::Int(n / d),
+					(Cell::Int(n), Cell::Flt(d)) => Cell::Flt(n as f64 / d),
+					(Cell::Flt(n), Cell::Int(d)) => Cell::Flt(n / d as f64),
+					(Cell::Flt(n), Cell::Flt(d)) => Cell::Flt(n / d),
+					_ => {				
+						// Type error:
+						panic!("Data type error. Type checking should have caught this earlier.");
+					},
+				}; // match					
+				if list.rest().is_empty(){
+					Ok(SExpression::Cell(partial_product))
+				}else{			
+					divide_into(partial_product, list.rest())		
+				}
+			} // Some(right_value)
+		} // match					
+	}
+
+
 		
 // Evaluate any  S-Expression
 pub fn evaluate(exp:SExpression)-> Result<SExpression,String>{		
@@ -61,9 +164,9 @@ pub fn evaluate(exp:SExpression)-> Result<SExpression,String>{
 		let not_implemented = String::from("Operator not implemented");
 		match func {
 			Add=> add(list),				
-			Subtract=>Err(not_implemented),
-			Multiply=> Err(not_implemented),
-			Divide=> Err(not_implemented),
+			Subtract=>subtract(list), 
+			Multiply=> multiply(list),
+			Divide=> divide(list),
 			Modulo=> Err(not_implemented),
 		}
 	}
