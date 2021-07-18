eddef77be3b6c358615f8c1628e40c4704714b8e
Colin Davis <ccd.umn.edu>
2021-07-18 15:57:58 -0500

    Stand-alone interpreter function

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,31 @@ fn number_list()->List{
 }
 
 
+// Put this in the REPL loop
+fn interpret(program:String)->String{
+	let tokens = parser::lex(program);
+	let p = parser::Parser::new();
+	match p.parse(&tokens){
+		Ok((valid_ast,_)) =>{		
+			// Parser seemed to work, so attempt to interpret the AST
+			let r = interpreter::evaluate(valid_ast);			
+			// Check for interpreter errors
+			match r {
+				Ok(good_result) => good_result.print(),
+				Err(error) => format!("Interpreter Error: {}", error),
+			}
+		},
+		// Parsing errors
+		Err(error) =>{
+			// handle different types of errors
+			match error{
+				parser::ParseError::Reason(reason) =>format!("{}", reason),
+			}
+		}
+	}
+}
+
+
 fn main() {
 	let n = number_list();	
 	let m = List::make_from_cells(vec![Cell::Int(5), Cell::Int(7), Cell::Str("abc".to_string())]);
@@ -32,37 +57,11 @@ fn main() {
 	println!("List of lists {}",p.print());
 	println!("sum of numbers: {}",summed.evaluate().unwrap().print());
 	
-	let tokens = parser::lex(String::from("(+ 1 2 3 (+ 5 6))"));
-	let p = parser::Parser::new();
-	match p.parse(&tokens){
-		Ok((valid_ast,_)) =>{		
-			// Parser seemed to work, so attempt to interpret the AST
-			let r = interpreter::evaluate(valid_ast);			
-			// Check for interpreter errors
-			match r {
-				Ok(good_result) => println!("Lex-parse-evaluate result is {}", good_result.print()),
-				Err(error) => println!("Interpreter Error: {}", error),
-			}
-		},
-		// Parsing errors
-		Err(error) =>{
-			// handle different types of errors
-			match error{
-				parser::ParseError::Reason(reason) =>println!("{}", reason),
-			}
-		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-    
-	
-	
+	println!("{}",interpret(String::from("(+ 1 2 3 (+ 5 6))")));
+	println!("{}",interpret(String::from("(+ 1 2 3 (* 5 6))")));
+	println!("{}",interpret(String::from("(/ 10 5)")));
+	println!("{}",interpret(String::from("(/ 5 10)")));
+	println!("{}",interpret(String::from("(* 8 (/ 5 10))")));
+	println!("{}",interpret(String::from("(- 8 (* 2 25) (+ 2 3) (/ 5 10))")));
 }
-
+	
