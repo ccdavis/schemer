4611a41f7b72a962eb5353d6f45305d5fee4a7ef
Colin Davis <ccd.umn.edu>
2021-11-13 18:13:20 -0600

    Work in progress: Sketching out a real (if primitive) scanner and parser

--- src/parser.rs
+++ src/parser.rs
@@ -15,7 +15,141 @@ pub enum ParseError {
   Reason(String),
 }
 
+enum TokenType{
+	StringLiteral(String),
+	LeftParen,
+	RightParen,
+	Other(String),	
+	EOF,
+}
 
+struct Token{
+	token_type:TokenType,
+	line:usize,
+	column:usize,	
+}
+
+
+// maintain the state of the lexical analysis plus keep track of column and line
+pub struct Lexer{
+	pos:usize,
+	line:usize,
+	column:usize,
+	text:String,	
+}
+
+impl Lexer{
+
+	fn end_of_input(&self)->bool{
+		self.pos == self.text.len()
+	}
+
+	fn new(text:String)->Self{
+		Self{
+			text:text,
+			pos:0,
+			line:1,
+			column:1,
+		}
+	}
+	
+	fn advance(&mut self){
+		if !self.end_of_input(){
+			self.pos += 1;
+			if self.this_char() == '\n'{ 
+				self.line += 1;
+				self.column = 1;
+			}else{
+				self.column += 1;
+			}						
+		}
+	}
+	
+	fn this_char(&self)->char{
+		self.text[self.pos]
+	}
+	
+	fn whitespace(&self)->bool{
+		self.this_char() == ' ' || self.this_char() == '\t'
+	}
+	
+	fn begin_comment(&self)->bool{
+		self.this_char() == ";"
+	}
+	
+	fn symbol_or_number_char(&self)->bool{
+		let c = self.this_char();
+		c.is_alphanumeric() || 
+		c == '+' || 
+		c == '-' || 
+		c == '/' || 
+		c == '*' || 
+		c == '%' ||
+		c == '\'' ||
+		c == '!'
+	}
+	
+	fn skip_whitespace(&mut self){		
+		while !self.end_of_input() && self.whitespace(){
+			self.advance();						
+		}
+	}
+
+	fn next(&mut self)->Token{
+		if self.begin_comment(){
+			while self.this_char() !='\n'  &&  !self.end_of_input(){
+				self.advance();
+			}
+		}
+		
+		self.skip_whitespace();
+		if self.end_of_input(){
+			return Token{token_type:EOF,line:self.line,column:self.column};
+		}
+				
+		let next_token = match self.this_char(){		
+			'(' =>{
+				self.advance();
+				Token {token_type:TokenType::LeftParen, line:self.line, column:self.column}
+			},
+			')' =>{
+				self.advance();
+				Token { token_type:TokenType::RightParen,line:self.line, column:self.column};
+			},
+			'"' =>{
+				let starting_line = self.line;
+				let starting_column = self.column;
+				
+				self.advance(); // eat first "
+				let mut content =  "".to_string();
+				while self.this_char()!='"'{
+					content.push(self.this_char());
+				}
+				
+				self.advance(); // eat the second " 
+				Token{token_type:StringLiteral(content), line:starting_line, column:starting_column}				
+			},
+			_=> {
+				if self.symbol_or_number_char() {
+					let mut content = "".to_string();
+					while self.symbol_or_number_char(){
+						content.push(self.this_char());
+					}
+					Token{token_type:Other(content),line:self.line,column:self.column}
+				}else{
+					// Something we didn't account for
+					// TODO: throw a real error!
+					panic!("Can't handle character '{}' at {}, {}",self.this_char(),self.line, self.column);										
+				}
+				
+			},			
+		};
+		self.advance();
+		
+		next_token
+	}
+
+}
 
 pub fn lex(expression: String) -> Vec<String> {
 // Ensure ( and ) are surrounded by whitespace, then
